bed2408947be184bf108ce2bb5b2fc67ee23fab3
thenaijagamer <sabdullahialaba50@gmail.com>
2024-09-20 12:32:07 +0100

    Implement some basic routing using POST and GET

--- app.ts
+++ app.ts
@@ -1,12 +1,31 @@
-import { Application, Request } from 'express';
-
-const express = require('express');
+import { Application } from 'express';
+import fs from 'fs';
+import express from 'express';
 
 const app: Application = express();
 
-app.get('/', (req, res) => {});
+app.use(express.json());
 
-const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('Done');
+});
+
+const port: number = 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
