21871b62bf311da982321a6acd172c484be59598
thenaijagamer <sabdullahialaba50@gmail.com>
2024-11-02 15:25:40 +0100

    Refactor the routing

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application } from 'express';
+import { Application, Response, Request } from 'express';
 import fs from 'fs';
 import express from 'express';
 
@@ -18,8 +18,9 @@ const tours: Tours[] = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
-// Get tours
-app.get('/api/v1/tours', (req, res) => {
+// __________Routes Handlers__________
+// Get tours route handler
+const getAllTours = (req: Request, res: Response) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -27,10 +28,10 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-// Post tours
-app.post('/api/v1/tours', (req, res) => {
+// Create tour route handler
+const createTour = (req: Request, res: Response) => {
   const id = tours[tours.length - 1].id + 1;
 
   const obj = Object.assign({ id }, req.body);
@@ -49,12 +50,10 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+};
 
-  // res.status(200).send('Done');
-});
-
-// Get tour by id
-app.get('/api/v1/tours/:id', (req, res): any => {
+// Get tour route handler
+const getTour = (req: Request, res: Response): any => {
   const tour = tours.find((tour) => tour.id === +req.params.id);
 
   if (!tour) {
@@ -71,10 +70,10 @@ app.get('/api/v1/tours/:id', (req, res): any => {
       tour,
     },
   });
-});
+};
 
-// Patch tour
-app.patch('/api/v1/tours/:id', (req, res): any => {
+// Update tour route handler
+const updateTour = (req: Request, res: Response): any => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'Invalid ID',
@@ -86,10 +85,10 @@ app.patch('/api/v1/tours/:id', (req, res): any => {
       tour: 'Update tour here',
     },
   });
-});
+};
 
-// Delete tour
-app.patch('/api/v1/tours/:id', (req, res): any => {
+// Delete tour route handler
+const deleteTour = (req: Request, res: Response): any => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'Invalid ID',
@@ -101,7 +100,26 @@ app.patch('/api/v1/tours/:id', (req, res): any => {
       tour: null,
     },
   });
-});
+};
+
+// // Get tours
+// app.get('/api/v1/tours', getAllTours);
+// // Post tours
+// app.post('/api/v1/tours', createTour);
+// // Get tour by id
+// app.get('/api/v1/tours/:id', getTour);
+// // Patch tour
+// app.patch('/api/v1/tours/:id', updateTour);
+// // Delete tour
+// app.patch('/api/v1/tours/:id', deleteTour);
+
+// More efficient way to handle routes
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port: number = 3000;
 
