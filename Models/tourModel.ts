54bd9ec6ab0a64fb6d83c0d59c90c6dd93985931
thenaijagamer <sabdullahialaba50@gmail.com>
2024-11-28 01:18:16 +0100

    Create a model for Tour

---
+++ Models/tourModel.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+interface ITour extends Document {
+  id: number;
+  name: string;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+  priceDiscount: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  createdAt: Date;
+  startDates: string[]; // ISO date strings with optional time
+}
+
+const tourSchema: Schema<ITour> = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+});
+
+const Tour: Model<ITour> = mongoose.model<ITour>('Tour', tourSchema);
+
+export default Tour;
