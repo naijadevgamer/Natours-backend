4d1d7467cfbc9e2ce11059674b1870d59c98f002
thenaijagamer <sabdullahialaba50@gmail.com>
2024-12-07 15:45:00 +0100

    Add the get monthly plan handler and modify the get montly stat

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -144,8 +144,8 @@ export const getTourStats = async (
 ): Promise<any> => {
   try {
     const stats = await Tour.aggregate([
-      { $match: { ratingsAverage: 4.5 } },
-      // { $match: { ratingsAverage: { $gte: 4.5 } } },
+      // { $match: { ratingsAverage: 4.5 } },
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
       {
         $group: {
           _id: { $toUpper: '$difficulty' },
@@ -157,12 +157,72 @@ export const getTourStats = async (
           maxPrice: { $max: '$price' },
         },
       },
+      {
+        $sort: { numTours: -1 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      results: Tour.length,
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err,
+    });
+  }
+};
+
+export const getMonthlyPlan = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const year = +req.params.year;
+    console.log(year);
+
+    const plans = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
     ]);
 
     res.status(200).json({
       status: 'success',
       data: {
-        stats,
+        plans,
       },
     });
   } catch (err) {
