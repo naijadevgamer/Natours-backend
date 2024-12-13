77b97d8b79d1732802eccefadb573d66d4075f53
thenaijagamer <sabdullahialaba50@gmail.com>
2024-12-13 01:30:37 +0100

    Create an utility class for appError

---
+++ utils/appError.ts
@@ -0,0 +1,9 @@
+export default class AppError extends Error {
+  statusCode;
+  status;
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+  }
+}
