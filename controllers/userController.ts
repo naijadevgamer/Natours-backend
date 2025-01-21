62ed09d7560f136a47a9117b02f38c3527ba2f79
thenaijagamer <sabdullahialaba50@gmail.com>
2025-01-21 03:37:56 +0100

    Implement get all users functionality

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,11 +1,21 @@
 import { Response, Request } from 'express';
+import catchAsync from '../utils/catchAsync';
+import User from '../Models/userModel';
 
-export const getAllUsers = (_req: Request, res: Response) => {
-  return res.status(500).json({
-    status: 'error',
-    messge: 'This route is not yet',
-  });
-};
+export const getAllUsers = catchAsync(
+  async (_req: Request, res: Response, _next) => {
+    const users = await User.find();
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  }
+);
 
 export const getUser = (_req: Request, res: Response) => {
   return res.status(500).json({
