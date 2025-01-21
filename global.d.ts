158b492bb8aab3f33306ceee76a44cb2d696f934
thenaijagamer <sabdullahialaba50@gmail.com>
2025-01-21 15:38:27 +0100

    Define more interface properties for ProcessEnv

--- global.d.ts
+++ global.d.ts
@@ -10,6 +10,8 @@ declare global {
       PORT: string; // Use string here
       DB_PASSWORD: string;
       DB: string;
+      JWT_SECRET: string;
+      JWT_SECRET_EXPIRATION: string;
     }
   }
 }
